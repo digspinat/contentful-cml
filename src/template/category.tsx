203de8078fc48f2d0d95347aae861910df98d830
Mitesh <cmilan@pistalix.com>
2017-12-11 11:25:23 +0530

    add grid with custom css and other filter

--- src/template/category.tsx
+++ src/template/category.tsx
@@ -1,16 +1,22 @@
 import * as React from "react";
 import { Container, Segment, Header, Icon, Grid, Divider, Image, Menu, Input, List } from "semantic-ui-react";
 import Link from "gatsby-link";
-import {InstantSearch, SearchBox, Hits, RefinementList, Pagination} from 'react-instantsearch/dom';
+import {InstantSearch, SearchBox, Hits, RefinementList, Pagination, Panel} from 'react-instantsearch/dom';
 
 function Searchcomp({hit, index}) {
   return (
-    <Grid.Column mobile={16} tablet={8} computer={1}>
-      <Link to={`manual/` + hit.manualSku} >
-        <Image src={hit.manualImgixUrlProduct} width="310px" height="401px" />
-        <Header as="h3" icon textAlign="center">{hit.manualTitle}</Header>
-      </Link>
-    </Grid.Column>
+    <article className="hit">
+      <div className="product-picture-wrapper">
+        <div className="product-picture">
+          <Image src={hit.manualImgixUrlProduct}  width="310px" height="401px"/>
+        </div>
+      </div>
+      <div className="product-desc-wrapper">
+        <div className="product-name">
+          <Header as="h3">{hit.manualTitle}</Header>
+        </div>
+      </div>
+    </article>
   );
 }
 
@@ -49,28 +55,46 @@ export default ({ data }) => {
         </Segment>
         <Segment vertical>
           <div>
+            <InstantSearch appId="YZO8M6AZ6J" apiKey="74516bbcef74ec917e2acbcd0df2df0f" indexName="contentful-cml">
+              <Grid>
+                <Grid.Column mobile={16} tablet={8} computer={4}>
+                  <Panel title="Brand">
+                    <RefinementList attributeName="partialAlgoliaMake" limitMax={20} limitMin={10} operator="or" showMore={false} defaultRefinement={[data.category.make]} withSearchBox={false} />
+                  </Panel><br />
+                  <Panel title="Series">
+                    <RefinementList attributeName="partialAlgoliaSerie" limitMax={20} limitMin={10} operator="or" showMore={false} withSearchBox={false} />
+                  </Panel><br />
+                  <Panel title="Sub Series">
+                    <RefinementList attributeName="partialAlgoliaSubSerie" limitMax={20} limitMin={10} operator="or" showMore={false} withSearchBox={false} />
+                  </Panel><br />
+                  <Panel title="Platform">
+                    <RefinementList attributeName="partialAlgoliaPlatform" limitMax={20} limitMin={10} operator="or" showMore={false} withSearchBox={false} />
+                  </Panel><br />
+                  <Panel title="Years">
+                    <RefinementList attributeName="partialAlgoliaYears" limitMax={20} limitMin={10} operator="or" showMore={false} withSearchBox={false} />
+                  </Panel><br />
+                  <Panel title="Fuel Type">
+                    <RefinementList attributeName="partialAlgoliaFuelType" limitMax={20} limitMin={10} operator="or" showMore={false} withSearchBox={false} />
+                  </Panel>
+                </Grid.Column>
+                <Grid.Column mobile={16} tablet={8} computer={12}>
+                  <SearchBox />
+                  <br /><br />
+                  <Hits hitComponent={Searchcomp} /><br />
+                </Grid.Column>
+              </Grid>
+              <Grid>
+                <Grid.Column mobile={16} tablet={8} computer={4}></Grid.Column>
+                <Grid.Column textAlign="center" mobile={16} tablet={8} computer={12}>
+                  <Pagination maxPages={3} pagesPadding={2} showFirst showLast showNext showPrevious />
+                </Grid.Column>
+              </Grid>
+            </InstantSearch>
+
             <Grid>
               <InstantSearch appId="YZO8M6AZ6J" apiKey="74516bbcef74ec917e2acbcd0df2df0f" indexName="contentful-cml">
-                <SearchBox />
-                <RefinementList
-                  attributeName="partialAlgoliaMake"
-                  limitMax={20}
-                  limitMin={10}
-                  operator="or"
-                  showMore={false}
-                  defaultRefinement={[data.category.make]}
-                  withSearchBox={false}
-                />
-                <Hits hitComponent={Searchcomp} />
+
                 <br />
-                <Pagination
-                   maxPages={3}
-                   pagesPadding={2}
-                   showFirst
-                   showLast
-                   showNext
-                   showPrevious
-                 />
               </InstantSearch>
             </Grid>
           </div>
